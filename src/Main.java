fe282eb6dbbadfd5fd559165f02073502f7f744a
adymar73583418 <adymarr2005@gmail.com>
2022-11-24 18:35:04 +0600

    gg

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ public class Main {
         Hero hero = new Hero(20,20,"защитный барьер");
         Hero spiderMan = new Hero(359,50);
         Boss boss = new Boss(430,100,"отталкивающий");
-        System.out.println(boss.getDmg() + boss.getHp() + hero.getDmg() + hero.getHp() +
-                spiderMan.getHp()+ spiderMan.getDmg());
+        System.out.println(boss.getDmg()+ " "+ boss.getHp() +boss.getProtection());
+        System.out.println(hero.getDmg()+ " "+ hero.getHp() +hero.getSuperpower());
+        System.out.println(spiderMan.getHp()+" " + spiderMan.getDmg());
 
     }
 }
